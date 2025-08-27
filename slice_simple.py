74acb376f066e161d1fe9635587c68cbaf32530d
riveirasanti <riveirasanti@gmail.com>
2025-08-27 09:00:59 -0300

    Add files via upload

--- slice_simple.py
+++ slice_simple.py
@@ -1,17 +1,12 @@
 def slice_simple():
     texto = "Awesome"
-    texto=texto.lower()
-    primeras_tres=texto[:3]
-    print(primeras_tres)
+    texto = texto.lower()
+    print(texto[:3])
 
-    medio=len(texto)// 2
-    medio_letras=texto[medio-1 : medio+2]
-    print(medio_letras)
-
-    primera_a_cuarta=texto[:4]
-    ultimas_tres=texto[-3:]
-    resultado=primera_a_cuarta + ultimas_tres
-    print(resultado)
+    medio=len(texto) // 2
+    print(texto[medio-1 : medio+2])
+    
+    print(texto[:4] + texto[-3:])
 
 slice_simple()
 
